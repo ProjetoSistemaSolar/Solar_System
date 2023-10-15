d38ea2b7067a8f2ae1f64b1c3916d64eba072b8a
Wendel_dev <107087269+Wendel-007@users.noreply.github.com>
2023-10-15 08:11:29 -0300

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,7 @@ import starsTexture from '../img/stars.jpg';
 import sunTexture from '../img/sun.jpg';
 import mercuryTexture from '../img/mercury.jpg';
 import venusTexture from '../img/venus.jpg';
-import moonTexture from '../img/venus.jpg';
+import moonTexture from '../img/moon.jpg';
 import earthTexture from '../img/earth.jpg';
 import marsTexture from '../img/mars.jpg';
 import jupiterTexture from '../img/jupiter.jpg';
